80decaf1becef16774bd3e6053f0b9972749eba1
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-03-23 20:22:44 -0500

    :construction: continued adding typeDefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,19 @@
+const { User, Thought } = require('../models')
+
+// a resolver function can accept up to 4 parameters in the following order:
+// parent: used for nested resolvers
+// args: values passed into the query or mutation request as params
+// context: for example, if we need to access a logged in user's status or API access token
+// info: extra info about the operation's current state.
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!'
+        thoughts: async (_, { username }) => {
+            const params = username ? { username } : {}
+            return Thought.find(params).sort({ createdAt: -1 })
+        },
+        thought: async (_, { _id }) => {
+            return Thought.findOne({ _id })
         }
     }
 }
