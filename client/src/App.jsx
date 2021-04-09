9c48256ca00dff355588c23321bd44bf315bb1a8
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-04-09 15:06:56 -0500

    :sparkles: added react-router-dom routes to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { ApolloProvider } from '@apollo/react-hooks'
 import ApolloClient from 'apollo-boost'
@@ -25,11 +25,15 @@ function App() {
       <div className='flex-column justify-flex-start min-100-vh'>
         <Header />
         <div className='container'>
+          <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/profile" component={Profile} />
           <Route exact path="/thought" component={SingleThought} />
+
+          <Route component={NoMatch} />
+          </Switch>
         </div>
         <Footer />
       </div>
